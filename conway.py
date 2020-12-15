29a525b95717e7b0dedd97c2d26c2359030a4d0d
christianTraylor <cchristiantraylor@gmail.com>
2020-12-14 19:41:28 -0600

    Add driver function for game logic and beginning of GUI code

---
+++ conway.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+class Cell():
+
+    def __init__(self,row,col,status):
+        self.row = row
+        self.col = col
+        self.status = status
+        self.false = 0
+        self.true = 0
+
+    def check(self, board):
+        count = 0
+        possibilities = (
+            (self.row-1,self.col-1), (self.row-1,self.col), (self.row-1,self.col+1), (self.row,self.col-1), 
+            (self.row,self.col+1), (self.row+1,self.col-1), (self.row+1,self.col), (self.row+1,self.col+1) 
+        )
+        if self.status:
+            for cord in possibilities:
+                if (cord[0] < 10) and (cord[1] < 10) and (cord[0] > -1) and (cord[1] > -1):
+                    if board[cord[0]][cord[1]].status == True:
+                        count+=1
+            if count == 2 or  count == 3:
+                return self.row,self.col,True
+            else:
+                return self.row,self.col,False
+        else:
+            for cord in possibilities:
+                if (cord[0] < 10) and (cord[1] < 10) and (cord[0] > -1) and (cord[1] > -1):
+                    if board[cord[0]][cord[1]].status == True:
+                        count+=1
+            if count == 3:
+                return self.row,self.col,True
+            else:
+                return self.row,self.col,False
+
+    def __repr__(self):
+        if self.status:
+            return "."
+        else:
+            return "0"
+
+class Board():
+    #TODO: Change seed to work with user input
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.board = np.zeros((self.x, self.y), dtype=Cell)
+        self.seed = int(np.random.randint(0,high=1000))
+
+    def populate(self):
+        np.random.seed(self.seed)
+        for x in range(self.x):
+            columnArr = np.random.randint(0,high=self.y,size=2)
+            for y in range(self.y):
+                if y in columnArr:
+                    self.board[x][y] = Cell(x,y,True)
+                else:
+                    self.board[x][y] = Cell(x,y,False)
+
+    def checkCells(self):
+        tempBoard = np.zeros((self.x,self.y), dtype=Cell)
+        for row in self.board:
+            for col in range(len(row)):
+                rowPos, colPos, status = row[col].check(self.board)
+                tempBoard[rowPos][colPos] = Cell(rowPos,colPos,status)
+        del self.board
+        self.board = tempBoard
+
+    def __repr__(self):
+            return str(self.board)
+
+
+board = Board(10,10)
+board.populate()
+print(board)
+print()
+print("1st gen")
+board.checkCells()
+print(board)
+
---
+++ display.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+class Window(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.master = master
+    # def create_widgets(self):
+    #     self.hi_there = tk.Button(self)
+    #     self.hi_there["text"] = "Hello World\n(click me)"
+    #     self.hi_there["command"] = self.say_hi
+    #     self.hi_there.pack(side="top")
+
+    #     self.quit = tk.Button(self, text="QUIT", fg="red",
+    #                           command=self.master.destroy)
+    #     self.quit.pack(side="bottom")
+
+    # def say_hi(self):
+    #     print("hi there, everyone!")s
+root = tk.Tk()
+root.geometry("500x500")
+app = Window(master=root)
+app.mainloop()
+
